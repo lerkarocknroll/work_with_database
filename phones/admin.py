ae2d0983252f46f013b7ba269cb12f26966c1265
Valeria <valeriazhitlukhina@gmail.com>
2025-11-08 17:46:40 +0500

    Выгрузка каталога товаров из CSV-файла

---
+++ phones/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from phones import models
+
+
+# Register your models here.
+@admin.register(models.Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'image', 'release_date', 'lte_exists', 'slug')
---
+++ phones/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Phone(models.Model):
+    # TODO: Добавьте требуемые поля
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=8, decimal_places=0)
+    image = models.ImageField()
+    release_date = models.DateField(auto_now=False, auto_now_add=False)
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(max_length=200, unique=True)
